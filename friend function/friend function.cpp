be013a6994f8d8d01c7297f0a98d51234477017d
abimanyusatriaa <adji3890@gmail.com>
2024-05-07 11:41:50 +0700

    menambah cout pada friend function pertama

--- friend function/friend function.cpp
+++ friend function/friend function.cpp
@@ -12,9 +12,10 @@ public:
     friend void setNama(mahasiswa& a, string);
 };
 
-void setNama(mahasiswa& a, string pNama)
+void setNama(mahasiswa& a, string b)
 {
-    a.nama = pNama;
+    a.nama = b;
+    cout << a.nama;
 }
 
 int main()
